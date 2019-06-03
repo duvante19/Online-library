05015b6d54a6c8594f73b217c751a119b9283bd9
duvante19 <35640077+duvante19@users.noreply.github.com>
2019-06-03 18:25:06 -0400

    Add files via upload

---
+++  onlinelibrary /img/media/data.php
@@ -0,0 +1,83 @@
+
+$catalog[101] = [
+  "title" => "the catcher in the rye",
+  "img" => "img/media/bookofjoy.jpg",
+ "genre" => "Novel, Bildungsroman, Coming-of-Age Fiction, First-person narrative",
+ "format" => "Paperback",
+ "year" => 1951,
+ "category" => "Books",
+ "authors" => [
+     "J. D. Salinger",
+ ],
+ "publisher" => "Little, Brown and Company",
+ "isbn" => '9781517712112'
+];
+
+
+$catalog[102] = [
+  "title" => "harry potter and the sorcerer's stone",
+  "img" => "img/media/bookofjoy.jpg",
+ "genre" => "Novel, Fantasy Fiction",
+ "format" => "Paperback",
+ "year" => 1997,
+ "category" => "Books",
+ "authors" => [
+     "J. K. Rowling",
+ ],
+ "publisher" => "Bloomsbury (UK) (Canada 2010–present)
+Arthur A. Levine/
+Scholastic (US)
+Raincoast (Canada 1998–2010)",
+ "isbn" => '0747532699'
+];
+
+*/Books*
+$catalog[103] = [
+  "title" => "Captain Underpants and the Perilous Plot of Professor Poopypants",
+  "img" => "img/media/bookofjoy.jpg",
+ "genre" => "Children's novel",
+ "format" => "Paperback",
+ "year" =>  2000 (black & white) December 29, 2015 (color edition) ,
+ "category" => "Books",
+ "authors" => [
+     "Dav Pilkey",
+ ],
+ "publisher" => "Blue Sky (US),
+Scholastic (US)",
+ "isbn" => '9780545871877'
+];
+
+*/Books*
+$catalog[104] = [
+  "title" => "Diary of a Wimpy Kid",
+  "img" => "img/media/bookofjoy.jpg",
+ "genre" => "Comedy, young adult fiction",
+ "format" => "Paperback",
+ "year" => 2007,
+ "category" => "Books",
+ "authors" => [
+     "	Jeff Kinney",
+ ],
+ "publisher" => "	Amulet Books",
+ "isbn" => '9780143303831'
+];
+
+*//Movies*
+$catalog[201] = [
+ "title" => "Avatar",
+ "img" => "img/media/avatar.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "James Cameron",
+ "writers" => [
+     "James Cameron"
+
+ ],
+ "stars" => [
+     "Sam Worthington",
+     "Zoe Saldana",
+     "Sigourney Weaver"
+ ]
+];
---
+++  onlinelibrary /inc/data.php
@@ -0,0 +1,166 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "the catcher in the rye",
+	"img" => "img/media/CatcherAndTheRye.jpg",
+    "genre" => "Novel, Bildungsroman, Coming-of-Age Fiction, First-person narrative",
+    "format" => "Paperback",
+    "year" => 1951,
+    "category" => "Books",
+    "authors" => [
+        "J. D. Salinger"
+    ],
+    "publisher" => "Little, Brown and Company",
+    "isbn" => '9780374111199'
+];
+$catalog[102] = [
+    "title" => "harry potter and the sorcerer's stone",
+    "img" => "img/media/HarryPotter.jpg",
+    "genre" => "Novel, Fantasy Fiction",
+    "format" => "Paperback",
+    "year" => 1997,
+    "category" => "Books",
+    "authors" => [
+        "J. K. Rowling"
+    ],
+    "publisher" => "Bloomsbury (UK) (Canada 2010–present)Arthur A. Levine",
+    "isbn" => '0747532699'
+];
+$catalog[103] = [
+    "title" => "Captain Underpants and the Perilous Plot of Professor Poopypants",
+    "img" => "img/media/CaptainUnderpants.jpg",
+    "genre" => "Children's novel",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Dav Pilkey"
+    ],
+    "publisher" => "Blue Sky (US)",
+    "isbn" => '9780545871877'
+];
+$catalog[104] = [
+    "title" => "Diary of a Wimpy Kid",
+    "img" => "img/media/DiaryofAWimpyKid.jpg",
+    "genre" => "Comedy, young adult fiction",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Jeff Kinney"
+    ],
+    "publisher" => "Amulet Books",
+    "isbn" => '9780143303831'
+];
+//Movies
+$catalog[201] = [
+    "title" => "transformers",
+    "img" => "img/media/Transformers.jpg",
+    "genre" => "Action,Adventure,Thriller,Science fiction",
+    "format" => "DVD",
+    "year" => 1986,
+    "category" => "Movies",
+    "director" => "Nelson Shin, Michael Bay",
+    "writers" => [
+        "Nelson Shin",
+        "Quentin Tarantino"
+    ],
+    "stars" => [
+        "Shia LaBeouf",
+        "Tyrese Gibson",
+        "Josh Duhamel",
+    ]
+];
+$catalog[202] = [
+    "title" => "Blood and bone",
+    "img" => "img/media/BloodAndBone.jpg",
+    "genre" => "Fantasy,Fantasy Fiction",
+    "format" => "DVD/Streaming",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Ben Ramsey",
+    "writers" => [
+        "Michael Andrews",
+				"Christopher Nolan"
+    ],
+    "stars" => [
+			"Michael Jai White",
+			"Julian Sands",
+			"Eamonn Walker"
+    ]
+];
+$catalog[203] = [
+    "title" => "Never Back Down",
+    "img" => "img/media/NeverBackDown.jpg",
+    "genre" => "Action,Adventure,Drama,Teen,Martial Arts,Coming of-age Fiction,Thriller,Sports",
+    "format" => "DVD",
+    "year" => 2008,
+    "category" => "Movies",
+    "director" => "Jeff Wadlow",
+    "writers" => [
+        "Chris Hauty",
+        "Lionel Wigram"
+    ],
+    "stars" => [
+			"Sean Faris",
+			"Amber Heard",
+			"Cam Gigandet"
+    ]
+];
+$catalog[204] = [
+    "title" => "The Players Club",
+		"img" => "img/media/PlayersClub.jpg",
+	  "genre" => "Comedy,Drama",
+	  "format" => "DVD/Streaming",
+	  "year" => 1998,
+	  "category" => "Movies",
+	  "director" => "Ice Cube",
+	  "writers" => [
+	      "Ice Cube"
+
+	  ],
+	  "stars" => [
+	      "LisaRaye",
+	      "Bernie Mac",
+	      "Monica Calhoun"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "BUTTERFLY EFFECT",
+	"img" => "img/media/TravisScott.jpg",
+	"genre" => "Hip-Hop/Rap",
+	"format" => "Digital Download",
+	"year" => 2017,
+	"category" => "Music",
+	"artist" => "Travis Scott"
+];
+
+$catalog[302] = [
+	"title" => "Sauce It Up",
+	"img" => "img/media/LilUziVert.jpg",
+	"genre" => "Hip-Hop/Rap",
+	"format" => "Digital Download",
+	"year" => 2017,
+	"category" => "Music",
+	"artist" => "Lil Uzi Vert"
+];
+$catalog[303] = [
+	"title" => "Eminem - Stan (Long Version) ft. Dido",
+	"img" => "img/media/eminem.jpg",
+	"genre" => "Hip-Hop/Rap",
+	"format" => "Digital Download",
+	"year" => 2007,
+	"category" => "Music",
+	"artist" => "Eminem"
+];
+$catalog[304] = [
+	"title" => "Spam Feat. Rich The Kid & Jay Critch",
+	"img" => "img/media/FamousDex.jpg",
+	"genre" => "Hip-Hop/Rap",
+	"format" => "Digital Download",
+	"year" => 2012,
+	"category" => "Music",
+	"artist" => "Famous Dex"
+];
